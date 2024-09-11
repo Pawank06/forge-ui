bd86c487d557ea15b433804d4ce334ee57d59dc2
PawanK06 <pawan06kumar2003@gmail.com>
2024-09-11 23:35:02 +0530

    add page.mdx files

---
+++ app/test/page.tsx
@@ -0,0 +1,82 @@
+import CodePreview from '@/components/code-preview'
+import TabAnimation from '@/components/TabAnimation'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='mt-20 w-[45rem] mx-auto'>
+        <CodePreview code={`
+'use client'
+
+import React, {useRef } from 'react'
+import { AnimationControls, motion, useAnimation } from 'framer-motion'
+
+const TabAnimation = () => {
+  const controls = useAnimation();
+
+  const handleMouseLeave = () => {
+    controls.start({ opacity: 0 });
+  };
+
+  return (
+    <div className='w-full flex items-center justify-center'>
+      <ul
+        onMouseLeave={handleMouseLeave}
+        className='relative border-2 mx-auto dark:bg-stone-950 border-black dark:border-stone-700 px-1 py-1 rounded-full flex items-center font-semibold'
+      >
+        <Tab controls={controls}>PRICING</Tab>
+        <Tab controls={controls}>PRODUCTS</Tab>
+        <Tab controls={controls}>FEATURES</Tab>
+        <Tab controls={controls}>DOCS</Tab>
+        <Tab controls={controls}>BLOG</Tab>
+        <Cursor controls={controls} />
+      </ul>
+    </div>
+  )
+}
+
+export default TabAnimation;
+
+const Tab = ({ children, controls }: { children: React.ReactNode, controls: AnimationControls }) => {
+  const ref = useRef<null | HTMLLIElement>(null);
+
+  const handleMouseEnter = () => {
+    if (!ref?.current) return;
+
+    const { width } = ref.current.getBoundingClientRect();
+
+    controls.start({
+      left: ref.current.offsetLeft,
+      width,
+      opacity: 1,
+    });
+  };
+
+  return (
+    <li
+      ref={ref}
+      onMouseEnter={handleMouseEnter}
+      className='relative z-10 block cursor-pointer px-3 py-1.5 text-xs uppercase text-white mix-blend-difference md:px-5 md:py-3 md:text-base'
+    >
+      {children}
+    </li>
+  );
+};
+
+const Cursor = ({ controls }: { controls: AnimationControls }) => {
+  return (
+    <motion.li
+      animate={controls}
+      className='absolute z-0 h-7 rounded-full dark:bg-stone-800 bg-black md:h-12'
+    />
+  );
+};
+
+`}>
+  <TabAnimation/>
+</CodePreview>
+    </div>
+  )
+}
+
+export default page
---
+++ app/test2/page.tsx
@@ -0,0 +1,12 @@
+import TabAnimation from '@/components/TabAnimation'
+import React from 'react'
+
+const Test2 = () => {
+  return (
+    <div className='w-full h-screen mx-auto flex items-center justify-center'>
+        <TabAnimation/>
+    </div>
+  )
+}
+
+export default Test2
